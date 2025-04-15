3e43951a47312f9cd80673b2d8fc21a2eb1a7c01
Het Patel <h.n.patel2412@gmail.com>
2025-04-15 19:25:37 +0530

    Updated project with final code

--- src/application.py
+++ src/application.py
@@ -1,43 +1,58 @@
-from flask import Flask,render_template,request,redirect
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import pickle
 import pandas as pd
 import numpy as np
 
-app=Flask(__name__)
-cors=CORS(app)
-model=pickle.load(open('LinearRegressionModel.pkl','rb'))
-car=pd.read_csv('Cleaned_Car_data.csv')
+app = Flask(__name__)
+CORS(app)
 
-@app.route('/',methods=['GET','POST'])
+# Load the model and dataset
+model = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
+car = pd.read_csv('Cleaned_Car_data.csv')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    companies=sorted(car['company'].unique())
-    car_models=sorted(car['name'].unique())
-    year=sorted(car['year'].unique(),reverse=True)
-    fuel_type=car['fuel_type'].unique()
+    companies = sorted(car['company'].unique())
+    car_models = sorted(car['name'].unique())
+    year = sorted(car['year'].unique(), reverse=True)
+    fuel_type = car['fuel_type'].unique()
 
-    companies.insert(0,'Select Company')
-    return render_template('index.html',companies=companies, car_models=car_models, years=year,fuel_types=fuel_type)
+    companies.insert(0, 'Select Company')
 
+    return render_template('index.html',
+                           companies=companies,
+                           car_models=car_models,
+                           years=year,
+                           fuel_types=fuel_type)
 
-@app.route('/predict',methods=['POST'])
+@app.route('/get_car_models', methods=['POST'])
+@cross_origin()
+def get_car_models():
+    data = request.get_json()
+    selected_company = data.get('company')
+    if selected_company:
+        filtered_models = sorted(car[car['company'] == selected_company]['name'].unique().tolist())
+        return jsonify({'models': filtered_models})
+    return jsonify({'models': []})
+
+@app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
+    company = request.form.get('company')
+    car_model = request.form.get('car_models')
+    year = int(request.form.get('year'))
+    fuel_type = request.form.get('fuel_type')
+    driven = int(request.form.get('kilo_driven'))
 
-    company=request.form.get('company')
+    # Create DataFrame for prediction
+    input_df = pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
+                            data=np.array([car_model, company, year, driven, fuel_type]).reshape(1, 5))
 
-    car_model=request.form.get('car_models')
-    year=request.form.get('year')
-    fuel_type=request.form.get('fuel_type')
-    driven=request.form.get('kilo_driven')
+    prediction = model.predict(input_df)
+    output = np.round(prediction[0], 2)
 
-    prediction=model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
-                              data=np.array([car_model,company,year,driven,fuel_type]).reshape(1, 5)))
-    print(prediction)
+    return render_template('result.html', predicted_price=output)
 
-    return str(np.round(prediction[0],2))
-
-
-
-if __name__=='__main__':
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
